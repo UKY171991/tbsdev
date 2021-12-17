cc01f6325115a76236e6292aa27b5d94f5ffce9d
Umakant <umakant171991@gmail.com>
2021-12-18 04:50:00 +0530

    atlenterpriseltd.com

---
+++ atlenterpriseltd.com/public_html/Same-Day-Delivery-Courier-Bicester.php
@@ -0,0 +1,267 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Europe Same Day Courier Delivery Service</title>
+	<meta name="description" content="Looking for same day courier delivery service? Connect with ATL enterprise ltd to get instant courier same day delivery in whole Europe with discounted rate">
+<meta name="keywords" content="same day courier delivery service, courier same day delivery, europe courier services">
+
+<link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+
+  <meta name="author" content="ATL Enterprise Ltd">
+	<!-- viewport meta -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- styles -->
+	<!-- helpers -->
+	<link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+	<!-- fontawesome css -->
+	<link rel="stylesheet" href="plugins/font-awesome/css/font-awesome.min.css">
+	<!-- strock gap icons -->
+	<link rel="stylesheet" href="plugins/Stroke-Gap-Icons-Webfont/style.css">
+	<!-- revolution slider css -->
+	<link rel="stylesheet" href="plugins/revolution/css/settings.css">
+	<link rel="stylesheet" href="plugins/revolution/css/layers.css">
+	<link rel="stylesheet" href="plugins/revolution/css/navigation.css">
+	<!-- flaticon css -->
+	<link rel="stylesheet" href="plugins/flaticon/flaticon.css">
+	<!-- jQuery ui css -->
+	<link href="plugins/jquery-ui-1.11.4/jquery-ui.css" rel="stylesheet">
+	<!-- owl carousel css -->
+	<link rel="stylesheet" href="plugins/owl.carousel-2/assets/owl.carousel.css">
+	<link rel="stylesheet" href="plugins/owl.carousel-2/assets/owl.theme.default.min.css">
+	<!-- animate css -->
+	<link rel="stylesheet" href="plugins/animate.min.css">
+	<!-- fancybox -->
+	<link rel="stylesheet" href="plugins/fancyapps-fancyBox/source/jquery.fancybox.css">
+
+
+
+	<!-- master stylesheet -->
+	<link rel="stylesheet" href="css/style.css">
+	<!-- responsive stylesheet -->
+	<link rel="stylesheet" href="css/responsive.css">
+
+
+
+</head>
+<body>
+
+    
+    
+<? include ('top.php'); ?>
+
+
+<section class="rev_slider_wrapper thm-banner-wrapper">
+	<div id="slider1" class="rev_slider"  data-version="5.0">
+		<ul>
+
+			
+
+			<li data-transition="parallaxvertical" data-ease="SlowMo.ease">
+				<img src="images/slide_1.jpg"  alt="1">
+				<div class="tp-caption sfb tp-resizeme caption-h1" 
+			        data-x="left" data-hoffset="0" 
+			        data-y="top" data-voffset="168" 
+			        data-whitespace="nowrap"
+			        data-transform_idle="o:1;" 
+			        data-transform_in="o:0" 
+			        data-transform_out="o:0" 
+			        data-start="500">
+					Courier Service!
+			    </div>
+				<div class="tp-caption sfb tp-resizeme caption-p" 
+			        data-x="left" data-hoffset="0" 
+			        data-y="top" data-voffset="235" 
+			        data-whitespace="nowrap"
+			        data-transform_idle="o:1;" 
+			        data-transform_in="o:0" 
+			        data-transform_out="o:0" 
+			        data-start="1000">
+					Banbury, Brackley, Bicester <br />Same Day Delivery<br />Full UK Coverage
+			    </div>
+				<div class="tp-caption sfl tp-resizeme" 
+			        data-x="left" data-hoffset="0" 
+			        data-y="top" data-voffset="340" 
+			        data-whitespace="nowrap"
+			        data-transform_idle="o:1;" 
+			        data-transform_in="o:0" 
+			        data-transform_out="o:0" 
+			        data-start="1500">
+					<a href="about-us.php" class="thm-btn">About us <i class="fa fa-arrow-right"></i></a>
+			    </div>
+			</li>
+
+			<li data-transition="parallaxvertical" data-ease="SlowMo.ease">
+				<img src="images/slide_1.jpg" alt="1">
+				
+                <!--
+				<div class="tp-caption sfb tp-resizeme caption-h1" 
+			        data-x="right" data-hoffset="0" 
+			        data-y="bottom" data-voffset="0" 
+			        data-whitespace="nowrap"
+			        data-transform_idle="o:1;" 
+			        data-transform_in="o:0" 
+			        data-transform_out="o:0" 
+			        data-start="300">
+					<img src="images/slider/cap-2.png" alt="1"/>
+			    </div>-->
+			    <div class="tp-caption sfb tp-resizeme caption-h1" 
+			        data-x="left" data-hoffset="10" 
+			        data-y="top" data-voffset="148" 
+			        data-whitespace="nowrap"
+			        data-transform_idle="o:1;" 
+			        data-transform_in="o:0" 
+			        data-transform_out="o:0" 
+			        data-start="800">
+					Courier Service!
+			    </div>
+				<div class="tp-caption sfb tp-resizeme caption-p" 
+			        data-x="left" data-hoffset="10" 
+			        data-y="top" data-voffset="235" 
+			        data-whitespace="nowrap"
+			        data-transform_idle="o:1;" 
+			        data-transform_in="o:0" 
+			        data-transform_out="o:0" 
+			        data-start="1100">
+					Banbury, Brackley, Bicester <br />Same Day Delivery<br />Full UK Coverage
+			    </div>
+			    <div class="tp-caption sfl tp-resizeme" 
+			        data-x="left" data-hoffset="10" 
+			        data-y="top" data-voffset="360" 
+			        data-whitespace="nowrap"
+			        data-transform_idle="o:1;" 
+			        data-transform_in="o:0" 
+			        data-transform_out="o:0" 
+			        data-start="1500">
+					<a href="about-us.php" class="thm-btn inverse">About us <i class="fa fa-arrow-right"></i></a>
+			    </div>
+			</li>
+		</ul>
+	</div>
+</section>
+
+<section class="about-info-box sec-padding">
+	<div class="thm-container">
+		<div class="row">
+			<div class="col-lg-4 hidden-md hidden-sm hidden-xs">
+				<div class="img-cap-effect">
+					<div class="img-box">
+						<img src="images/about-info-box/2.jpg" alt="Awesome Image"/>
+						<div class="img-caption">
+							<div class="box-holder"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-8 col-md-12">
+				<div class="sec-title">
+					<h3>Benefits of Using Same Day Delivery Courier Bicester</h3>
+					
+<p class="p1">We all should consider ourselves lucky that we live in an era where sending and receiving things is so easy. All we have to do is hire a courier bisector, pay a small fee and deliver our parcels and packages anywhere. Similarly, we can receive packages from all over the world in little to no time. Isn&rsquo;t it fun? No one likes to wait for the package, be it online shopping or someone sending you a gift; the joy is real!<span class="Apple-converted-space">&nbsp;</span></p>
+<p class="p3"><strong>Benefits of Using Same Day Delivery Banbury</strong></p>
+<p class="p1">There are many advantages of using a same-day delivery Bicester. First of all, they help you deliver or receive a parcel within 24 hours. It helps you meet all the deadlines and saves you a lot of time. Also, it saves you a lot of money. You only have to pay <a href="http://atlenterpriseltd.com/services.php"><strong>courier Banbury</strong></a> fees and keep the petrol charges! Other than that, the chances of losing your documents, etc., are also less when you decide to use <strong>courier Bicester.<span class="Apple-converted-space">&nbsp;</span></strong></p>
+<p class="p1">We all need to know about a few such courier companies because we never know when an emergency might arise, and we have to send urgent documents to someone. Not to forget you can use such delivery services to send gifts to your loved ones.</p>
+<p class="p3"><strong>Hire the Best Same Day Delivery Bicester</strong></p>
+<p class="p1">Hire &ldquo;ALT Enterprise LTD&rdquo;<span class="Apple-converted-space">&nbsp; </span>for courier services because they are bound to deliver your package within the next 24 hours. The best part of same-day delivery Banbury is that they are open 24 hours, which means the services are not limited to a day, night or weekdays only.</p>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+
+<? include('bottom.php'); ?>
+    
+
+
+<!-- jQuery js -->
+<script src="plugins/jquery/jquery-1.11.3.min.js"></script>
+<!-- bootstrap js -->
+<script src="plugins/bootstrap/js/bootstrap.min.js"></script>
+<!-- jQuery ui js -->
+<script src="plugins/jquery-ui-1.11.4/jquery-ui.js"></script>
+<!-- owl carousel js -->
+<script src="plugins/owl.carousel-2/owl.carousel.min.js"></script>
+<!-- jQuery appear -->
+<script src="plugins/jquery-appear/jquery.appear.js"></script>
+<!-- jQuery countTo -->
+<script src="plugins/jquery-countTo/jquery.countTo.js"></script>
+<!-- jQuery validation -->
+<script src="plugins/jquery-validation/dist/jquery.validate.min.js"></script>
+<!-- gmap.js helper -->
+<script src="http://maps.google.com/maps/api/js"></script>
+<!-- gmap.js -->
+<script src="plugins/gmap.js"></script>
+<!-- mixit up -->
+<script src="plugins/jquery.mixitup.min.js"></script>
+
+<!-- revolution slider js -->
+<script src="plugins/revolution/js/jquery.themepunch.tools.min.js"></script>
+<script src="plugins/revolution/js/jquery.themepunch.revolution.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.migration.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.video.min.js"></script>
+
+<!-- fancy box -->
+<script src="plugins/fancyapps-fancyBox/source/jquery.fancybox.pack.js"></script>
+<!-- type script -->
+<script src="plugins/typed.js-master/dist/typed.min.js"></script>
+
+<!-- theme custom js  -->
+<script src="js/main.js"></script>
+
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "NewsArticle",
+  "mainEntityOfPage": {
+    "@type": "WebPage",
+    "@id": "http://atlenterpriseltd.com/"
+  },
+  "headline": "same day courier delivery services in Banbury, Brackley, Bicester",
+  "image": "",  
+  "author": {
+    "@type": "Organization",
+    "name": "ATL Enterprise Ltd"
+  },  
+  "publisher": {
+    "@type": "Organization",
+    "name": "",
+    "logo": {
+      "@type": "ImageObject",
+      "url": ""
+    }
+  },
+  "datePublished": "2020-11-26",
+  "dateModified": "2020-11-26"
+}
+</script>
+
+</body>
+</html>
